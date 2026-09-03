5cae67a223c9a8e9c706b7ce80d7d106ee16eee9
mohitjain610 <mj353534@gmail.com>
2026-09-03 16:31:33 +0530

    Time: 1 ms (68.38%), Space: 42.6 MB (95.23%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        char [] arr=new char[s.length()];
+        int top=-1;
+        for(int i=0;i<s.length();i++){
+            if(top>=0 && arr[top]=='(' && s.charAt(i)==')')top--;
+            else arr[++top]=s.charAt(i);
+        }
+        return top+1;
+    }
+}
